1e89adfc2e1fddb9c17668223ab540c35a60c4dd
muaazshoaib <muaazshoaib1670@gmail.com>
2023-04-22 15:32:33 +0500

    ...

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,9 +1,15 @@
+import 'dart:developer';
+
+import 'package:camera/camera.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 
 import '../components/my_body_text.dart';
 import '../components/my_button.dart';
 import '../components/my_heading.dart';
+import '../models.dart';
+import 'detect_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -13,6 +19,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final List<CameraDescription> cameras;
+
+  @override
+  Future<void> didChangeDependencies() async {
+    super.didChangeDependencies();
+    await setupCameras();
+  }
+
+  loadModel(model) async {
+    String? res;
+    switch (model) {
+      case yolo:
+        res = await Tflite.loadModel(
+          model: "assets/yolov2_tiny.tflite",
+          labels: "assets/yolov2_tiny.txt",
+        );
+        break;
+
+      case mobilenet:
+        res = await Tflite.loadModel(
+            model: "assets/mobilenet_v1_1.0_224.tflite",
+            labels: "assets/mobilenet_v1_1.0_224.txt");
+        break;
+
+      case posenet:
+        res = await Tflite.loadModel(
+            model: "assets/posenet_mv1_075_float_from_checkpoints.tflite");
+        break;
+
+      default:
+        res = await Tflite.loadModel(
+            model: "assets/ssd_mobilenet.tflite",
+            labels: "assets/ssd_mobilenet.txt");
+    }
+    log("$res");
+  }
+
+  onSelect(model) {
+    loadModel(model);
+    final route = MaterialPageRoute(builder: (context) {
+      return DetectPage(cameras: cameras, model: model);
+    });
+    Navigator.of(context).push(route);
+  }
+
+  setupCameras() async {
+    try {
+      cameras = await availableCameras();
+    } on CameraException catch (e) {
+      log('Error: $e.code\nError Message: $e.message');
+    }
+  }
+
   final user = FirebaseAuth.instance.currentUser!;
 
   // sign user out method
@@ -35,6 +94,28 @@ class _HomePageState extends State<HomePage> {
                 MyBodyText(text: 'Logged in as ${user.email!}'),
 
                 const SizedBox(height: 20),
+
+                MyButton(
+                  buttonText: 'SSD',
+                  onTap: () => onSelect(ssd),
+                ),
+                const SizedBox(height: 20),
+                MyButton(
+                  buttonText: 'YOLO',
+                  onTap: () => onSelect(yolo),
+                ),
+                const SizedBox(height: 20),
+
+                MyButton(
+                  buttonText: 'MOBILENET',
+                  onTap: () => onSelect(mobilenet),
+                ),
+                const SizedBox(height: 20),
+                MyButton(
+                  buttonText: 'POSENET',
+                  onTap: () => onSelect(posenet),
+                ),
+                const SizedBox(height: 50),
 
                 // sign out button
                 MyButton(
